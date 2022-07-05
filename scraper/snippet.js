40d7d38867a5bbc11f7dca253e1907e563f02897
pkrakas <pkrakas@gmail.com>
2022-07-05 18:06:03 +0200

    Reformated code and added timeout

--- scraper/snippet.js
+++ scraper/snippet.js
@@ -1,3 +1,36 @@
+async function timeout() {
+    return new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 3000) + 2000)) // Between 2-5s
+}
+
+async function runScraper() {
+    let nextPageAnchor
+
+    while (nextPageAnchor = $("a[data-cy='pagination-forward']")[0]) {
+
+        const offerAnchors = $("div[data-cy='l-card'] a").toArray()
+        const offers = offerAnchors.map(offer => {
+            const url = offer.href
+            textSplit = offer.innerText.split('\n\n')
+            const title = textSplit[0].split('\n')[1] || textSplit[0]
+            const price = textSplit[1].split('\n')[0]
+            const location = textSplit[2].split('-')[0].trim()
+
+            return {
+                url,
+                title,
+                price,
+                location
+            }
+        })
+
+
+
+        nextPageAnchor.click()
+        await waitForElement('div[data-testid="listing-grid"]')
+        await timeout()
+    }
+}
+
 function waitForElement(selector) {
     return new Promise(resolve => {
         if (document.querySelector(selector)) {
@@ -26,36 +59,7 @@ function waitForElement(selector) {
     jqueryScript.src = 'https://code.jquery.com/jquery-3.6.0.min.js'
     document.getElementsByTagName('head')[0].appendChild(jqueryScript)
 
-    jqueryScript.addEventListener('load', async () => {
-
-        let nextPageAnchor
-
-        while(nextPageAnchor = $("a[data-cy='pagination-forward']")[0]) {
-
-            const offerAnchors = $("div[data-cy='l-card'] a").toArray()
-            const offers = offerAnchors.map(offer => {
-                const url = offer.href
-                textSplit = offer.innerText.split('\n\n')
-                const title = textSplit[0].split('\n')[1] || textSplit[0]
-                const price = textSplit[1].split('\n')[0]
-                const location = textSplit[2].split('-')[0].trim()
-
-                return {
-                    url,
-                    title,
-                    price,
-                    location
-                }
-            })
-
-
-            
-            nextPageAnchor.click()
-            await waitForElement('div[data-testid="listing-grid"]')
-
-        }
-        
-    })
+    jqueryScript.addEventListener('load', runScraper)
 
 })()
 
